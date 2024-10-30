acd83fabd0121252b950ebd1492d6be9232293b9
Doggy <pedan.denys@gmail.com>
2024-10-30 22:37:25 +1300

    Notes save on memory. No DB

---
+++ app/src/main/java/denys/diomaxius/todoapp/TodoListPage.kt
@@ -0,0 +1,121 @@
+package denys.diomaxius.todoapp
+
+import android.media.midi.MidiManager.OnDeviceOpenedListener
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+@Composable
+fun TodoListPage(viewModel: TodoViewModel) {
+    val todoList by viewModel.todList.observeAsState(emptyList())
+
+    var inputText by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            OutlinedTextField(
+                modifier = Modifier,
+                value = inputText,
+                onValueChange = { inputText = it }
+            )
+            Button(onClick = {
+                viewModel.addTodo(inputText)
+                inputText = ""
+            }) {
+                Text(text = "Add")
+            }
+        }
+        LazyColumn() {
+            items(todoList) {
+                TodoItem(
+                    item = it,
+                    onDelete = {viewModel.deleteTodo(it.id)}
+                    )
+            }
+        }
+    }
+}
+
+@Composable
+fun TodoItem(
+    item: Todo,
+    onDelete: () -> Unit
+    ) {
+    Row(
+        modifier = Modifier
+            .heightIn(min = 75.dp)
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(Color(0xff3a336e)),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(15.dp)
+                .weight(1f)
+        ) {
+            Text(
+                text = SimpleDateFormat("HH:mm:aa, dd/mm", Locale.ENGLISH).format(item.createdAt),
+                fontSize = 10.sp,
+                color = Color.LightGray
+            )
+            Text(
+                text = item.title,
+                fontSize = 20.sp,
+                color = Color.White
+            )
+        }
+        IconButton(onClick = onDelete) {
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Rabbish bin",
+                tint = Color.White
+            )
+        }
+    }
+}
